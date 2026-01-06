7d033cf9051ab9595455bf942b544629af614daf
Manimaran <manimaran25060@gmail.com>
2026-01-06 13:20:22 +0530

    Add MainActivity base

---
+++ app/src/main/java/com/tcmaran/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tcmaran.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("TCMARAN App Started");
+        setContentView(tv);
+    }
+}
